c5733f8e34ad48ee52670d3400e6633571bf7c81
Robotics <1loverobots!>
2023-11-06 17:23:43 -0800

    did some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/assemblies/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/assemblies/Auto.java
@@ -1,4 +1,36 @@
 package org.firstinspires.ftc.teamcode.assemblies;
 
-public class Auto {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.libs.TeamGamepad;
+import org.firstinspires.ftc.teamcode.libs.teamUtil;
+
+@Disabled
+@TeleOp(name = "Auto", group = "LinearOpMode")
+public class Auto extends LinearOpMode {
+    double middleThreshold = 300; // tenative
+    double leftThreshold = 300; // tenative
+    Robot robot;
+    @Override
+    public void runOpMode(){
+        teamUtil.init(this);
+        robot = new Robot();
+        robot.initialize();
+        robot.output.grabOnePixel();
+        teamUtil.pause(1000);
+        robot.calibrate();
+        robot.drive.initCV();
+        robot.drive.runSideTeamPropFinderProcessor();
+        while(!opModeIsActive()){
+            robot.drive.visionTelemetry();
+        }
+        waitForStart();
+        while (opModeIsActive()) {
+
+
+        }
+    }
 }
+
