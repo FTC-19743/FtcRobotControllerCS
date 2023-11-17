28ae3249a3ad12b576bd0b3f85b814be018bd788
Robotics <ftc19743@gmail.com>
2023-11-17 10:19:33 -0800

    Added buttons and limit switches to hibble

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hibble/HibbleMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hibble/HibbleMotors.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.libs.teamUtil;
@@ -12,8 +13,23 @@ public class HibbleMotors {
     HardwareMap hardwareMap;
     Telemetry telemetry;
 
+    TouchSensor touch;
+
+    public TouchSensor upperLimit;
+    public TouchSensor lowerLimit;
+    public TouchSensor upButton;
+    public TouchSensor downButton;
     public DcMotorEx motorZero;
     public DcMotorEx motorOne;
+
+    boolean upButtonWasPressed = false;
+    boolean upButtonWasPressedLastTime = false;
+    boolean upButtonBumpToDo = false;
+
+    boolean downButtonWasPressed = false;
+    boolean downButtonWasPressedLastTime = false;
+    boolean downButtonBumpToDo = false;
+
 
     public HibbleMotors() {
         teamUtil.log("Constructing Hibble Motors");
@@ -28,11 +44,78 @@ public class HibbleMotors {
         motorOne = hardwareMap.get(DcMotorEx.class, "motor_one");
         motorZero.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Initializes Limit Switches
+        upperLimit = hardwareMap.get(TouchSensor.class, "upper_limit");
+        lowerLimit = hardwareMap.get(TouchSensor.class,"lower_limit");
+        upButton = hardwareMap.get(TouchSensor.class, "up_button");
+        downButton = hardwareMap.get(TouchSensor.class, "down_button");
+
     }
 
     public void moveMotors(double power){
         motorZero.setPower(power);
         motorOne.setPower(power);
     }
+
+    public void moveUpTillLimit(){
+        moveMotors(-1);
+        while(!isUpperLimitSwitchTriggered()){}
+        moveMotors(0);
+    }
+
+    public void moveDownTillLimit(){
+        moveMotors(1);
+        while(!isLowerLimitSwitchTriggered()){}
+        moveMotors(0);
+    }
+
+    public boolean isUpperLimitSwitchTriggered(){
+        if (upperLimit.isPressed()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean isLowerLimitSwitchTriggered(){
+        if(lowerLimit.isPressed()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void wasDownButtonPressed(){
+        if(downButtonWasPressedLastTime == true && downButton.isPressed() == false){
+            downButtonWasPressed = true;
+            downButtonWasPressedLastTime = false;
+        }
+        else if(downButton.isPressed() == true){
+            downButtonWasPressed = false;
+            downButtonWasPressedLastTime = true;
+        }
+        else{
+            downButtonWasPressed = false;
+            downButtonWasPressedLastTime = false;
+        }
+    }
+
+    public void wasUpButtonPressed(){
+        if(upButtonWasPressedLastTime == true && upButton.isPressed() == false){
+            upButtonWasPressed = true;
+            upButtonWasPressedLastTime = false;
+        }
+        else if(upButton.isPressed() == true){
+            upButtonWasPressed = false;
+            upButtonWasPressedLastTime = true;
+        }
+        else{
+            upButtonWasPressed = false;
+            upButtonWasPressedLastTime = false;
+        }
+    }
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hibble/HibbleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hibble/HibbleOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.hibble;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.libs.TeamGamepad;
 import org.firstinspires.ftc.teamcode.libs.teamUtil;
@@ -12,6 +13,7 @@ import org.firstinspires.ftc.teamcode.libs.teamUtil;
 public class HibbleOpMode extends LinearOpMode {
     TeamGamepad gamepad;
     HibbleMotors hibbleMotors;
+    TouchSensor touch;
     @Override
 
     public void runOpMode(){
@@ -23,12 +25,46 @@ public class HibbleOpMode extends LinearOpMode {
         teamUtil.init(this);
         waitForStart();
         while (opModeIsActive()) {
+
+            //Test for if limitswitch is triggered
+            if(hibbleMotors.isUpperLimitSwitchTriggered()){
+                telemetry.addLine("UpperTriggered");
+                teamUtil.log("Upper Limit Switch Triggered");
+            }
+            if(hibbleMotors.isLowerLimitSwitchTriggered()){
+                telemetry.addLine("LowerTriggered");
+                teamUtil.log("Lower Limit Switch Triggered");
+            }
+
+            //moves motors up till magnetic switch is triggered
+            if(gamepad1.dpad_up){
+                hibbleMotors.moveUpTillLimit();
+            }
+
+            if(gamepad1.dpad_down){
+                hibbleMotors.moveDownTillLimit();
+            }
+
+            //this is manual control over hibble
             gamepad.loop();
             if(Math.abs(gamepad1.left_stick_y) < .1){
                 hibbleMotors.moveMotors(0);
             }
             else{
                 hibbleMotors.moveMotors(gamepad1.left_stick_y);
+            }
+
+            //button press test for hibble
+
+            hibbleMotors.wasDownButtonPressed();
+            hibbleMotors.wasUpButtonPressed();
+            if(hibbleMotors.downButtonWasPressed == true){
+                teamUtil.log("Down Button Was Pressed");
+                hibbleMotors.moveDownTillLimit();
+            }
+            if(hibbleMotors.upButtonWasPressed == true){
+                teamUtil.log("Up Button Was Pressed");
+                hibbleMotors.moveUpTillLimit();
             }
         }
     }
