c241824d8b789c1539cd6a666d3b63efa20ea63b
Robotics <1loverobots!>
2023-12-01 14:05:00 -0800

    debugging multiple vision portals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/calibrateCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/calibrateCV.java
@@ -54,6 +54,7 @@ public class calibrateCV extends LinearOpMode
     // Manually set the camera gain and exposure.  Camera must be actively streaming
     // Returns true if controls are set.
     private boolean updateCVManualExposure(VisionPortal portal, int exposureMS, int gain) {
+        teamUtil.log("Setting Manual Exposure");
         // Ensure Vision Portal has been setup.
         if (portal == null) {
             return false;
@@ -76,9 +77,12 @@ public class calibrateCV extends LinearOpMode
             // Set exposure.  Make sure we are in Manual Mode for these values to take effect.
             ExposureControl exposureControl = portal.getCameraControl(ExposureControl.class);
             if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                teamUtil.log("Switching to Manual Mode");
                 exposureControl.setMode(ExposureControl.Mode.Manual);
                 sleep(50);
             }
+            teamUtil.log("Setting Exposure");
+
             exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
             sleep(20);
 
@@ -86,12 +90,30 @@ public class calibrateCV extends LinearOpMode
             GainControl gainControl = portal.getCameraControl(GainControl.class);
             if (gainControl == null) { // FTC software has a bug where this isn't supported on switchableCameras!
             } else {
+                teamUtil.log("Setting Gain");
                 gainControl.setGain(gain);
             }
             sleep(20);
+            teamUtil.log("Setting Manual - Finished");
             return (true);
         } else {
+            teamUtil.log("Setting Manual - Finished");
             return (false);
+        }
+    }
+
+    // Assumes portal is either currently STREAMING or in process of starting STREAMING
+    public void stopStreaming(VisionPortal portal) {
+        teamUtil.log("Stop Streaming ");
+
+        while (!isStopRequested() && (portal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+            teamUtil.log("Waiting for Camera to finish coming on line: " + portal.getCameraState());
+            teamUtil.pause(100);
+        }
+        portal.stopStreaming();
+        while (!isStopRequested() && (portal.getCameraState() != VisionPortal.CameraState.CAMERA_DEVICE_READY)) {
+            teamUtil.log("Waiting for Camera to stop: " + portal.getCameraState());
+            teamUtil.pause(100);
         }
     }
     public void initCV(boolean enableLiveView) { //should be false for comp code
@@ -111,48 +133,63 @@ public class calibrateCV extends LinearOpMode
             sideCam = hardwareMap.get(WebcamName.class, "Webcam Left");
         }
 
+        teamUtil.log("Getting Multiportal");
+
         // In order to build multiple VisionPortals, you must first set up a MultiPortalView, even if you don't plan to run them at the same time
         int[] visionPortalViewIDs = VisionPortal.makeMultiPortalView(3, VisionPortal.MultiPortalLayout.HORIZONTAL);
 
+
         // Set up rear Camera
+        teamUtil.log("Setting up rearVisionPortal");
         VisionPortal.Builder rearBuilder = new VisionPortal.Builder();
         rearBuilder.setCamera(backCam);
         rearBuilder.setLiveViewContainerId(visionPortalViewIDs[0]);
         rearBuilder.enableLiveView(enableLiveView);
         rearBuilder.setAutoStopLiveView(true);
+        rearBuilder.addProcessor(aprilTag);
         // Can also set resolution and stream format if we want to optimize resource usage.        rearBuilder.addProcessor(aprilTag);
         rearVisionPortal = rearBuilder.build();
+        stopStreaming(rearVisionPortal);
 
         // Set up side Camera
+        teamUtil.log("Setting up sideVisionPortal");
         VisionPortal.Builder sideBuilder = new VisionPortal.Builder();
         sideBuilder.setCamera(backCam);
         sideBuilder.setLiveViewContainerId(visionPortalViewIDs[1]);
         sideBuilder.enableLiveView(enableLiveView);
         sideBuilder.setAutoStopLiveView(true);
-        // Can also set resolution and stream format if we want to optimize resource usage.
         sideBuilder.addProcessor(findTeamPropProcesser);
+        // Can also set resolution and stream format if we want to optimize resource usage.
         sideVisionPortal = sideBuilder.build();
+        stopStreaming(sideVisionPortal);
 
         // Set up front Camera
+        teamUtil.log("Setting up frontVisionPortal");
         VisionPortal.Builder frontBuilder = new VisionPortal.Builder();
         frontBuilder.setCamera(backCam);
         frontBuilder.setLiveViewContainerId(visionPortalViewIDs[2]);
         frontBuilder.enableLiveView(enableLiveView);
         frontBuilder.setAutoStopLiveView(true);
+        frontBuilder.addProcessor(findLineProcesser);
         // Can also set resolution and stream format if we want to optimize resource usage.        frontBuilder.addProcessor(findLineProcesser);
         frontVisionPortal = frontBuilder.build();
+        stopStreaming(frontVisionPortal);
 
         teamUtil.log("Initializing Drive CV - FINISHED");
     }
 
     public void stopCV() {
+        teamUtil.log("Stopping any running Cams");
         if (rearVisionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+            teamUtil.log("Stopping Rear Cam");
             rearVisionPortal.stopStreaming();
         }
         if (frontVisionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+            teamUtil.log("Stopping Front Cam");
             frontVisionPortal.stopStreaming();
         }
         if (sideVisionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+            teamUtil.log("Stopping Side Cam");
             frontVisionPortal.stopStreaming();
         }
         aprilTagProcessorRunning = false;
@@ -160,7 +197,10 @@ public class calibrateCV extends LinearOpMode
         findTeamPropProcessorRunning = false;
     }
     public void switchCV(cvCam newCam) {
+        teamUtil.log("Switching CV to " + newCam);
         stopCV();
+        teamUtil.log("Starting " + newCam);
+
         switch (newCam) {
             case REAR_APRILTAG:
                 rearVisionPortal.resumeStreaming();
@@ -175,18 +215,19 @@ public class calibrateCV extends LinearOpMode
                 findTeamPropProcessorRunning = true;
                 break;
         }
+        teamUtil.log("switchCV - Finished ");
     }
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
     // Code specific to calibrateCV op mode
     int currentCam = 1;
-    private int[]     myExposure  ;
-    private int[]     minExposure ;
-    private int[]     maxExposure ;
-    private int[]     myGain      ;
-    private int[]     minGain ;
-    private int[]     maxGain ;
+    private int[]     myExposure = new int[4] ;
+    private int[]     minExposure = new int[4] ;
+    private int[]     maxExposure = new int[4] ;
+    private int[]     myGain = new int[4]      ;
+    private int[]     minGain = new int[4]  ;
+    private int[]     maxGain = new int[4]  ;
     public void toggleCV() {
         currentCam++;
         if (currentCam > 3) {
@@ -219,6 +260,7 @@ public class calibrateCV extends LinearOpMode
             }
         } else if (findLineProcessorRunning) {
             telemetry.addLine("FrontCam:" + frontVisionPortal.getCameraState()+ " FPS:" + frontVisionPortal.getFps());
+            telemetry.addLine("Threshold: " + findLineProcesser.whiteThreshold);
             findLineProcesser.outputTelemetry();
         }
         else if (findTeamPropProcessorRunning) {
@@ -243,19 +285,17 @@ public class calibrateCV extends LinearOpMode
             teamUtil.telemetry.update();
         }
 
-        initCV(true);
+        initCV(false); // All portals and cameras are now ready, nothing is streaming
         // Establish Min and Max Gains and Exposure.  Then set a low exposure with high gain
         getAllCameraSettings();
-        for (int cam = 1;cam < 4;cam++)
-        {
-            myExposure[cam] = Math.min(5, minExposure[cam]);
-            myGain[cam] = maxGain[cam];
-            updateCVManualExposure(cam==1 ? rearVisionPortal : cam==2 ? frontVisionPortal : sideVisionPortal, myExposure[cam], myGain[cam]);
-        }
 
-        // All 3 cams are running at this point
+        // No Cams are streaming but several might be shutting down
         switchCV(cvCam.REAR_APRILTAG);
         currentCam = 1;
+        while (!isStopRequested() && (rearVisionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+            teamUtil.log("Waiting for rear camera to come on line: " + rearVisionPortal.getCameraState());
+            sleep(100);
+        }
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -263,6 +303,8 @@ public class calibrateCV extends LinearOpMode
 
         while (opModeIsActive())
         {
+            gamepad.loop();
+
             telemetry.addLine("Use Left bump/trig to adjust Exposure.");
             telemetry.addLine("Use Right bump/trig to adjust Gain.\n");
             writeTelemetry();
@@ -290,7 +332,7 @@ public class calibrateCV extends LinearOpMode
                     // TODO: adjust saturation thresholds and maybe rectangles
                 }
                 if (currentCam==2) {
-                    findLineProcesser.whiteThreshold--;
+                    findLineProcesser.whiteThreshold++;
                 }
             }
             if (gamepad.wasDownPressed()) {
@@ -298,8 +340,11 @@ public class calibrateCV extends LinearOpMode
                     // TODO: adjust saturation thresholds
                 }
                 if (currentCam==2) {
-                    findLineProcesser.whiteThreshold++;
+                    findLineProcesser.whiteThreshold--;
                 }
+            }
+            if (gamepad.wasYPressed()){
+                toggleCV();
             }
 
             sleep(20);
@@ -308,16 +353,23 @@ public class calibrateCV extends LinearOpMode
 
     public void getAllCameraSettings() {
         teamUtil.log("Getting Rear Camera Settings");
-        getCameraSettings(1,rearVisionPortal);
+        rearVisionPortal.resumeStreaming();
+        getCameraSettings(1,rearVisionPortal, 4,250);
+        rearVisionPortal.stopStreaming();
         teamUtil.log("Getting Front Camera Settings");
-        getCameraSettings(2,frontVisionPortal);
+        frontVisionPortal.resumeStreaming();
+        getCameraSettings(2,frontVisionPortal, findLineProcesser.lineExposure, 250);
+        frontVisionPortal.stopStreaming();
         teamUtil.log("Getting Side Camera Settings");
-        getCameraSettings(3,sideVisionPortal);
+        sideVisionPortal.resumeStreaming();
+        getCameraSettings(3,sideVisionPortal, findTeamPropProcesser.propExposure, 250);
+        sideVisionPortal.stopStreaming();
     }
 
-    private void getCameraSettings(int cam, VisionPortal portal) {
+    private void getCameraSettings(int cam, VisionPortal portal, int startExposure, int startGain) {
         // Ensure Vision Portal has been setup.
         if (portal == null) {
+            teamUtil.log("Null portal!");
             return;
         }
 
@@ -326,7 +378,8 @@ public class calibrateCV extends LinearOpMode
             telemetry.addData("Camera", "Waiting");
             telemetry.update();
             while (!isStopRequested() && (portal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
-                sleep(20);
+                teamUtil.log("Waiting for camera to come on line");
+                sleep(100);
             }
             telemetry.addData("Camera", "Ready");
             telemetry.update();
@@ -334,10 +387,12 @@ public class calibrateCV extends LinearOpMode
 
         // Get camera control values unless we are stopping.
         if (!isStopRequested()) {
+            teamUtil.log("getting exposure control limits");
             ExposureControl exposureControl = portal.getCameraControl(ExposureControl.class);
             minExposure[cam] = (int)exposureControl.getMinExposure(TimeUnit.MILLISECONDS) + 1;
             maxExposure[cam] = (int)exposureControl.getMaxExposure(TimeUnit.MILLISECONDS);
 
+            teamUtil.log("getting gain control limits");
             GainControl gainControl = portal.getCameraControl(GainControl.class);
             if (gainControl == null) { // FTC software has a bug where this isn't supported on switchableCameras!
                 minGain[cam] = 0;
@@ -347,5 +402,8 @@ public class calibrateCV extends LinearOpMode
                 maxGain[cam] = gainControl.getMaxGain();
             }
         }
+        myExposure[cam] = Math.min(5, minExposure[cam]);
+        myGain[cam] = maxGain[cam];
+        updateCVManualExposure(portal, myExposure[cam], myGain[cam]);
     }
 }
