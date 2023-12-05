82d2c7da6bfade80be1b897c44a489bc7eb97142
ftc19743 <ftc19743@gmail.com>
2023-12-05 10:49:10 -0800

    Updated to work on both Alliances Tried to fix Blue side teleop heading bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/testAutoPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/testAutoPaths.java
@@ -32,6 +32,18 @@ public class testAutoPaths extends LinearOpMode {
         robot.intake.initalize();
         robot.output.initialize();
         robot.output.calibrate();
+        while(!driverGamepad.wasAPressed()){
+            driverGamepad.loop();
+            if(driverGamepad.wasLeftPressed()){ teamUtil.alliance = teamUtil.Alliance.RED;}
+            if(driverGamepad.wasRightPressed()){ teamUtil.alliance = teamUtil.Alliance.BLUE;}
+
+            teamUtil.telemetry.addLine("RED or BLUE? (use Game Pad 1 DPad)");
+            teamUtil.telemetry.addLine(teamUtil.alliance == teamUtil.Alliance.RED ? "RED Alliance" : "BLUE Alliance");
+            teamUtil.telemetry.addLine("------------------------------------");
+            teamUtil.telemetry.addLine("Then press A on Game Pad 1 to move on");
+            teamUtil.telemetry.update();
+        }
+
         telemetry.addLine("Ready to start");
         telemetry.update();
         robot.drive.setHeading(180);
@@ -44,41 +56,44 @@ public class testAutoPaths extends LinearOpMode {
 
             ////////// Drive
             if (driverGamepad.gamepad.right_stick_button && driverGamepad.gamepad.left_stick_button) {
-                robot.drive.setHeading(180);            }
-            robot.drive.universalDriveJoystick(
-                    driverGamepad.gamepad.left_stick_y,
-                    -driverGamepad.gamepad.left_stick_x,
-                    driverGamepad.gamepad.right_stick_x,
-                    driverGamepad.gamepad.right_trigger>.5,
-                    robot.drive.getHeading());
-
+                robot.drive.setHeading(180);
+            }
+            if (teamUtil.alliance == teamUtil.Alliance.RED) {
+                robot.drive.universalDriveJoystick(
+                        driverGamepad.gamepad.left_stick_y,
+                        -driverGamepad.gamepad.left_stick_x,
+                        driverGamepad.gamepad.right_stick_x,
+                        driverGamepad.gamepad.right_trigger > .5,
+                        robot.drive.getHeading());
+            } else {
+                robot.drive.universalDriveJoystick(
+                        -driverGamepad.gamepad.left_stick_y,
+                        driverGamepad.gamepad.left_stick_x,
+                        driverGamepad.gamepad.right_stick_x,
+                        driverGamepad.gamepad.right_trigger > .5,
+                        robot.drive.getHeading());
+            }
             if(driverGamepad.wasLeftPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.SCORE;
                 robot.autoV3(1, false);
             }
             if(driverGamepad.wasUpPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.SCORE;
                 robot.autoV3(2, true);
             }
             if(driverGamepad.wasRightPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.SCORE;
                 robot.autoV3(3, false);
             }
             if(driverGamepad.wasXPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.WING;
                 robot.autoV3(1, true);
             }
             if(driverGamepad.wasYPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.WING;
                 robot.autoV3(2, true);
             }
             if(driverGamepad.wasBPressed()) {
-                teamUtil.alliance= teamUtil.Alliance.RED;
                 teamUtil.SIDE=teamUtil.Side.WING;
                 robot.autoV3(3, true);
             }
