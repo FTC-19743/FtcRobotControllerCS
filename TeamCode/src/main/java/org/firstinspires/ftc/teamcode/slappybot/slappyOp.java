249e6dd3f14b79494e574e6590a75f3cde687930
Robotics <1loverobots!>
2023-12-11 15:48:33 -0800

    SlappyBot Moves to a Control Hub!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slappybot/slappyOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slappybot/slappyOp.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.slappybot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.libs.TeamGamepad;
@@ -11,10 +13,15 @@ import org.firstinspires.ftc.vision.VisionPortal;
 
 
 
-@TeleOp(name = "slappyOp", group = "LinearOpMode")
+@TeleOp(name = "SlappyOp", group = "LinearOpMode")
 public class slappyOp extends LinearOpMode {
     TeamGamepad gamepad;
-    WebcamName hibbleMotors;
+    DcMotorEx leftMotor, rightMotor;
+    Servo headServo;
+    VisionPortal visionPortal;
+
+    DcMotorEx armMotor;
+
     @Override
 
     public void runOpMode() {
@@ -22,17 +29,36 @@ public class slappyOp extends LinearOpMode {
         gamepad = new TeamGamepad();
         gamepad.initilize(true);
 
-
-        VisionPortal visionPortal;
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        visionPortal = builder.build();
+        setUp();
 
         waitForStart();
 
         while (opModeIsActive()) {
-
+            gamepad.loop();
+            drive (gamepad.gamepad.left_stick_x, gamepad.gamepad.left_stick_y);
+            if (Math.abs(gamepad.gamepad.right_stick_y)> .1f) {
+                armMotor.setPower(gamepad.gamepad.right_stick_y);
+            } else {
+                armMotor.setPower(0);
+            }
+            turnHead(gamepad.gamepad.left_trigger, gamepad.gamepad.right_trigger);
         }
         visionPortal.close();
+    }
+
+    public void setUp() {
+        leftMotor = hardwareMap.get(DcMotorEx.class, "lm");
+        rightMotor = hardwareMap.get(DcMotorEx.class, "rm");
+        armMotor = hardwareMap.get(DcMotorEx.class, "am");
+        headServo = hardwareMap.get(Servo.class,"headServo");
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        visionPortal = builder.build();
+    }
+    public void drive (float x, float y) {
+
+    }
+    public void turnHead (float left, float right) {
+
     }
 }
